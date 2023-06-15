4d337c200c42f37f7a0d2b54aa09eabb9ec73f0c
Bayaneben <bayanebenhassan95@gmail.com>
2023-06-15 13:59:06 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Description: it display the size of a variable
@@ -7,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of char: %lu bytes(s)\n", sizeof(char));
-	printf("size of int: %lu bytes(s)\n", sizeof(int));
-	printf("size of a long int: %lu bytes(s)\n", sizeof(long int));
-	printf("size of long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("size of float: %lu bytes(s)\n", sizeof(float));
+	printf("size of char: %lu byte(s)\n", sizeof(char));
+	printf("size of int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
