5198f77ea6b8d4a7ae513b17341b6258d3eaf3a1
Bayaneben <bayanebenhassan95@gmail.com>
2023-06-22 07:44:43 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *mul - makes the multiplication between two numbers
+  *@a: first num
+  *@b: second num
+  * Return: return multiplicattion
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_FILE
 int _isupper(int c);
 int _putchar(char c);
+int mul(int a, int b);
 int _isdigit(int c);
 #endif
